9ae723dec14f1bb7979ffdadd7ccda772905c125
Johannes Ekberg <uppfinnarn@gmail.com>
2013-05-04 02:26:41 +0200

    Broken encryption \o/

--- secretbooru.py
+++ secretbooru.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # encoding: utf-8
-import os, re
+import os, re, struct
 import urllib2
 from datetime import datetime
 from flask import Flask, session, request, g
 from flask import url_for, redirect, abort, render_template, flash
 from pysqlcipher import dbapi2
 from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
 from Crypto import Random
 
 app = Flask(__name__)
@@ -16,20 +17,27 @@ app.config.from_object('secrets')
 site_root = os.path.abspath(os.path.dirname(__file__))
 path = lambda filename: os.path.join(site_root, filename)
 
+# http://stackoverflow.com/questions/12562021/aes-decryption-padding-with-pkcs5-python
+BS = AES.block_size
+pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
+unpad = lambda s : s[0:-ord(s[-1])]
+
 class Post(object):
 	id = 0
 	added = None
 	rating = 'q'
 	mime = 'text/plain'	#lolnope
-	key = None
+	key = ''
 	
-	basequery = "SELECT ROWID, added, rating, mime, key FROM posts"
+	base_query = "SELECT ROWID, added, rating, mime, key FROM posts"
+	date_format = '%Y-%m-%d %H:%M:%S'
 	
-	def __init__(self, id, added, rating, mime, key):
+	def __init__(self, id=-1, added=datetime.now(), rating='q', mime='', key=None):
 		self.id = id
-		self.added = datetime.strptime(added, '%Y-%m-%d %H:%M:%S')
-		self.mime = mime
+		self.added = added#datetime.strptime(added, self.date_format)
 		self.rating = rating
+		self.mime = mime
+		self.key = key
 	
 	def path(self, thumb=False):
 		suffix = ''
@@ -37,11 +45,27 @@ class Post(object):
 	
 	def get_data(self, thumb=False):
 		with open(self.path(thumb)) as f:
-			return f.read()
+			iv = 'a'*AES.block_size #f.read(AES.block_size)
+			cipher = AES.new(self.key, AES.MODE_CBC, iv)
+			data = cipher.decrypt(unpad(f.read()))
+			return data
 	
 	def set_data(self, data, format, thumb=False, make_thumb=True):
+		c = g.db.cursor()
+		rand = Random.new()
+		self.key = rand.read(32)
+		iv = 'a'*AES.block_size #rand.read(AES.block_size)
+		c.execute("UPDATE posts SET key = ? WHERE ROWID = ?", (buffer(self.key), self.id))
+		
+		cipher = AES.new(self.key, AES.MODE_CBC, iv)
+		cipherdata = cipher.encrypt(pad(data))
+		
 		with open(self.path(thumb), 'wb') as f:
+			f.write(iv)
 			f.write(data)
+		
+		g.db.commit()
+		
 		if not thumb and make_thumb:
 			self.make_thumbnail(data, format)
 	
@@ -56,10 +80,21 @@ class Post(object):
 		img.save(d, format)
 		self.set_data(d.getvalue(), format, thumb=True)
 	
+	def save(self):
+		c = g.db.cursor()
+		if self.id < 0:
+			c.execute("INSERT INTO posts (added, rating, mime) VALUES (?, ?, ?)",
+				(self.added, self.rating, self.mime))
+			self.id = c.lastrowid
+		else:
+			c.execute("UPDATE posts SET added = ?, rating = ?, mime = ? WHERE ROWID = ?",
+				(self.added, self.rating, self.mime, self.id))
+		g.db.commit()
+	
 	@classmethod
 	def get(cls, id):
 		c = g.db.cursor()
-		rec = c.execute(cls.basequery + " WHERE ROWID = ?", [id]).fetchone()
+		rec = c.execute(cls.base_query + " WHERE ROWID = ?", [id]).fetchone()
 		if rec is None:
 			return None
 		return cls(*rec)
@@ -68,7 +103,7 @@ class Post(object):
 	def all(cls):
 		c = g.db.cursor()
 		l = []
-		for rec in c.execute(cls.basequery).fetchall():
+		for rec in c.execute(cls.base_query).fetchall():
 			l.append(cls(*rec))
 		return l
 
@@ -162,27 +197,24 @@ def import_():
 		remote = urllib2.urlopen(request.form['url'])
 		info = remote.info()
 		mime = info['Content-Type']
-		key = Random.new().read(64)
+		#key = Random.new().read(64)
 		
-		c = g.db.cursor()
-		c.execute("INSERT INTO posts (rating, mime, key) VALUES (?, ?, ?)", ('q', mime, buffer(key)))
-		id = c.lastrowid
+		#c = g.db.cursor()
+		#c.execute("INSERT INTO posts (rating, mime) VALUES (?, ?)", ('q', mime))
+		#id = c.lastrowid
 		
-		post = Post.get(id)
+		#post = Post.get(id)
+		#post.set_data(remote.read(), mime.split('/')[-1])
+		
+		#g.db.commit()
+		
+		post = Post()
+		post.mime = mime
+		post.save()
+		
 		post.set_data(remote.read(), mime.split('/')[-1])
-		#with open(path('media/%s' % id), 'wb') as local:
-		#	data = remote.read()
-		#	local.write(data)
-		#	
-		#	img = Image.open(StringIO(data))
-		#	img.thumbnail((200, 200), Image.ANTIALIAS)
-		#	img.save(path('media/%s_thumb' % id), mime.split('/')[-1])
-		#	
-		#	del img
-		#	del data
 		
-		g.db.commit()
-		return redirect(url_for('post', id=id))
+		return redirect(url_for('post', id=post.id))
 	return render_template('import.html')
 
 if __name__ == '__main__':
